7181584ca0b5495851118bbe7b970a768274ee3b
raymond@rishty.net <raymond@rishty.net@ccefdb41-f44a-0410-beb1-2d3591274484>
2008-05-09 21:52:13 +0000

    Issue 84

--- trunk/ddl/findEventsFuture.sql
+++ trunk/ddl/findEventsFuture.sql
@@ -1,8 +1,9 @@
-CREATE PROCEDURE findEventsFuture
+
+CREATE  PROCEDURE findEventsFuture
 @channelIdSpecial tinyint,
 @channelIdRegular tinyint
 AS
-SELECT     i.item_id, i.pubDate, i.title, id.description, i.author, i.channel_id channelId
+SELECT     i.item_id, i.pubDate, i.title, id.description, i.author, i.channel_id channelId, i.all_day_event allDayEvent
 FROM         item AS i INNER JOIN
                       item_description AS id ON i.item_id = id.item_id
 WHERE     (i.channel_id = @channelIdSpecial) OR (i.channel_id = @channelIdRegular) AND (i.pubDate >= GETDATE())
