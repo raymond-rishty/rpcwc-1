7317822410dbba2c46cb574f4b6854426de5b672
raymond@rishty.net <raymond@rishty.net@ccefdb41-f44a-0410-beb1-2d3591274484>
2008-05-28 05:48:51 +0000

    Issue 89

---
+++ trunk/App_Code/dao/RecommendedReadingsDAO.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Configuration;
+using System.Data.SqlClient;
+
+/// <summary>
+/// Summary description for RecommendedReadingsDAO
+/// </summary>
+public class RecommendedReadingsDAO
+{
+    private static string markReadingAsReadCommandString = "UPDATE ITEM SET ALL_DAY_EVENT = NULL WHERE ITEM_ID = @itemId";
+    private static SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["RPC"].ConnectionString);
+
+	public RecommendedReadingsDAO()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+
+    public void MarkAsRead(Int16 itemId)
+    {
+        connection.Open();
+        SqlCommand markReadingAsReadCommand = new SqlCommand(markReadingAsReadCommandString, connection);
+        markReadingAsReadCommand.Parameters.AddWithValue("itemId", itemId);
+        markReadingAsReadCommand.ExecuteNonQuery();
+        connection.Close();
+
+    }
+}
