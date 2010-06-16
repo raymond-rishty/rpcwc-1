2825b31343a9d726d2b7f5ce7f1c2bba73546c14
raymondofrish <raymondofrish@ccefdb41-f44a-0410-beb1-2d3591274484>
2010-06-16 22:41:15 +0000

    Try to find enclosure if unspecified Fix embedded player

---
+++ App_Code/util/WebUtil.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net;
+
+namespace rpcwc.util
+{
+    public class WebUtil
+    {
+        public static bool IsURLExtant(String url)
+        {
+
+            HttpWebRequest req = (HttpWebRequest)HttpWebRequest.Create(url);
+            req.Method = "HEAD";
+
+            try
+            {
+                HttpWebResponse response = (HttpWebResponse)req.GetResponse();
+                return (response.StatusCode == HttpStatusCode.OK);
+            }
+            catch (System.Net.WebException exception)
+            {
+                return (exception.Status == WebExceptionStatus.Success);
+            }
+        }
+    }
+}
